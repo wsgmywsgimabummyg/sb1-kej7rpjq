155635f4cec00000d9307cf3341493346a555d77
wsgmywsgimabummyg <josephhabte47@gmail.com>
2025-05-21 15:00:17 -0700

    Fix ScrollArea hydration and infinite update issues

--- components/ui/scroll-area.tsx
+++ components/ui/scroll-area.tsx
@@ -2,7 +2,6 @@
 
 import * as React from 'react';
 import * as ScrollAreaPrimitive from '@radix-ui/react-scroll-area';
-
 import { cn } from '@/lib/utils';
 
 const ScrollArea = React.forwardRef<
@@ -40,7 +39,10 @@ const ScrollBar = React.forwardRef<
     )}
     {...props}
   >
-    <ScrollAreaPrimitive.ScrollAreaThumb className="relative flex-1 rounded-full bg-border" />
+    <ScrollAreaPrimitive.ScrollAreaThumb 
+      className="relative flex-1 rounded-full bg-border"
+      style={{ position: 'relative' }} // Ensure stable positioning
+    />
   </ScrollAreaPrimitive.ScrollAreaScrollbar>
 ));
 ScrollBar.displayName = ScrollAreaPrimitive.ScrollAreaScrollbar.displayName;
